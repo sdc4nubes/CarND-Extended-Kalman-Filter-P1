eda3ee23eebc0b7025a249e3bada8d3406a0fb8b
rons-git <ron@cancunron.com>
2019-02-15 16:22:07 -0500

    update from windows

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -134,12 +134,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
         ekf_.R_ = R_radar_;
         Hj_     = tools.CalculateJacobian(ekf_.x_);
         ekf_.H_ = Hj_;
-        ekf_.Update(measurement_pack.raw_measurements_);
+        ekf_.UpdateEKF(measurement_pack.raw_measurements_);
     } else {
         // Laser updates
         ekf_.R_ = R_laser_;
         ekf_.H_ = H_laser_;
-        ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+        ekf_.Update(measurement_pack.raw_measurements_);
     }
     // print the output
     // cout << "x_ = " << ekf_.x_ << endl;
